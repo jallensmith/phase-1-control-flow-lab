3bd224f84219c827fcaabaaaac5d732902f1cc0b
Jerome Allen-Smith <jeromeallen99@gmail.com>
2022-07-09 12:44:20 -0400

    commit tree ae1db38d1be0548b7992504286a93ce60cfe5b0f

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet > 400 && feet < 2000) {
+    return 'That will be twenty bucks.'
+  } else if (feet > 2000 && feet < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500){
+    return 'No can do.'
+  }
+}
+  // const travelerMinDistance = 400;
+  // const travelerMaxDistance = 2000;
+  // let travelerDistance;
+  //if (travelerMinDistance <= 400) {
+  //  travelerDistance = "This one is on me!"
+  // }
+  // else if (travelerMinDistance > 400 && travelerMaxDistance < 2000){
+  //  travelerDistance = "That will be twenty bucks."
+  // }
+  //else if (travelMaxDistance > 2000)
+  //  travelerDistance = "I will gladly take your thirty bucks."
+  // }
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous" :
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
